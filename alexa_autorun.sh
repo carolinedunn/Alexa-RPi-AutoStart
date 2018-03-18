dc6bb11fbc301f071c5e0b03d998e00f38f4bf0d
Caroline Dunn <thecarolinedunn@gmail.com>
2018-03-18 10:29:00 -0400

    same files as the zip

---
+++ alexa_autorun.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+
+# Open Companion Service
+cd ~/alexa-avs-sample-app/samples/companionService && npm start & 
+
+# Open Java Client
+cd ~/alexa-avs-sample-app/samples/javaclient && mvn exec:exec & 
+
+# Wait
+sleep 25s
+
+# Open wakeWord Agent
+cd ~/alexa-avs-sample-app/samples/wakeWordAgent/src && ./wakeWordAgent -e kitt_ai &
+
+# Say "Hello" Audio
+cd ~/alexa-avs-sample-app && aplay hello.wav & 
+
+
+
+
+
+
+
+
+
+
+
+
+
